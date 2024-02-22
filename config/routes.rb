6b22f156076c3ce28582cbb58416d2b8c5070d65
Susan Prestage <sprestage@oneclickpolitics.com>
2024-02-22 13:01:48 -0500

    try using the client_domain model

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
 
   # constraints(domain: 'tabarahranchtrakehners.com') do
   constraints(ClientDomain) do
-    namespace :tabarahranchtrakehners do
+    # namespace :tabarahranchtrakehners do
       resources :home, only: [:index]
+      get '/index', to: 'client#index'
 
       root to: 'home#index'
-    end
+    # end
   end
 end
