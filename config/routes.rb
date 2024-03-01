a44b515cbff022763ec65fa4f22e55b1c0efc211
Susan Prestage <sprestage@oneclickpolitics.com>
2024-03-01 17:47:47 -0500

    adding secondary page to client

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'tabarahranchtrakehners/home', to: 'tabarahranchtrakehners/home#index'
+  get 'tabarahranchtrakehners/contact', to: 'tabarahranchtrakehners/home#contact'
 
   constraints(domain: 'tabarahranchtrakehners.com') do
     root to: 'tabarahranchtrakehners/home#index', as: 'tabarah_home_root'
+    get '/contact' => 'tabarahranchtrakehners/home#contact'
   end
 
   root 'welcome#index'
