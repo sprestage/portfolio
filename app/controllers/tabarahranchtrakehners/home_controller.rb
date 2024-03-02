4adbe016af443164814f9d92d5c2f479575bde22
Susan Prestage <sprestage@oneclickpolitics.com>
2024-03-02 00:18:55 -0500

    Adding the trt views

--- app/controllers/tabarahranchtrakehners/home_controller.rb
+++ app/controllers/tabarahranchtrakehners/home_controller.rb
@@ -1,4 +1,18 @@
 class Tabarahranchtrakehners::HomeController < ApplicationController
+  layout 'tabarahranchtrakehners/application'
+
   def index
+  end
+
+  def jaedorin_pedigree
+  end
+
+  def jaegerin_pedigree
+  end
+
+  def jakira_pedigree
+  end
+
+  def joriah_pedigree
   end
 end
--- app/helpers/tabarahranchtrakehners/home_helper.rb
+++ app/helpers/tabarahranchtrakehners/home_helper.rb
@@ -1,7 +1,23 @@
 module Tabarahranchtrakehners::HomeHelper
   # app/helpers/tabarahranchtrakehners/home_helper.rb
 
-  %w[home_root herd contact].each do |path|
+  paths = %w[
+    home_root
+    herd
+    forsale
+    pedigrees
+    contact
+    joriah
+    jakira
+    jaedorin
+    jaegerin
+    dorin_pedigree
+    gerin_pedigree
+    kira_pedigree
+    jori_pedigree
+  ]
+
+  paths.each do |path|
     define_method("#{path}_path") do
       if Rails.env.development?
         send("tabarahranchtrakehners_#{path}_path")
