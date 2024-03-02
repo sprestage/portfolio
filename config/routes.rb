838bd086871668af975e7f57aad67cafc9eb7738
Susan Prestage <sprestage@oneclickpolitics.com>
2024-03-01 22:38:46 -0500

    local vs prod routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'tabarahranchtrakehners/home', to: 'tabarahranchtrakehners/home#index'
+  get 'tabarahranchtrakehners/contact', to: 'tabarahranchtrakehners/home#contact'
 
   constraints(domain: 'tabarahranchtrakehners.com') do
     root to: 'tabarahranchtrakehners/home#index', as: 'tabarah_home_root'
