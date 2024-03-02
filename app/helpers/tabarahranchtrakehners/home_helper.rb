9d5011c40acb1ddf77a4e143e6d8aafb01c3ebe7
Susan Prestage <sprestage@oneclickpolitics.com>
2024-03-01 23:34:21 -0500

    use a DRYer version of that helper

--- app/helpers/tabarahranchtrakehners/home_helper.rb
+++ app/helpers/tabarahranchtrakehners/home_helper.rb
@@ -1,19 +1,13 @@
 module Tabarahranchtrakehners::HomeHelper
-# app/helpers/tabarahranchtrakehners/home_helper.rb
+  # app/helpers/tabarahranchtrakehners/home_helper.rb
 
-  def tabarah_home_root_path
-    if Rails.env.development?
-      tabarahranchtrakehners_home_path
-    else
-      super
-    end
-  end
-
-  def contact_path
-    if Rails.env.development?
-      tabarahranchtrakehners_contact_path
-    else
-      super
+  %w[home_root herd contact].each do |path|
+    define_method("#{path}_path") do
+      if Rails.env.development?
+        send("tabarahranchtrakehners_#{path}_path")
+      else
+        super()
+      end
     end
   end
 end
