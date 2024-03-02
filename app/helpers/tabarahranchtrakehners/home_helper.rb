92d51bccd9562a95b6aee8254c153597655ed292
Susan Prestage <sprestage@oneclickpolitics.com>
2024-03-01 23:07:17 -0500

    more local vs prod

--- app/helpers/tabarahranchtrakehners/home_helper.rb
+++ app/helpers/tabarahranchtrakehners/home_helper.rb
@@ -1,2 +1,19 @@
 module Tabarahranchtrakehners::HomeHelper
+# app/helpers/tabarahranchtrakehners/home_helper.rb
+
+  def tabarah_home_root_path
+    if Rails.env.development?
+      tabarahranchtrakehners_home_path
+    else
+      super
+    end
+  end
+
+  def contact_path
+    if Rails.env.development?
+      tabarahranchtrakehners_contact_path
+    else
+      super
+    end
+  end
 end
